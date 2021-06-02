c276e51a563ed1a731033f49b05c00d9ff5df2cd
WhiteFang <jorisschroeven@hotmail.com>
2021-06-02 19:53:48 +0200

    Updated appveyor build scripts

--- appveyor-build.sh
+++ appveyor-build.sh
@@ -14,4 +14,4 @@ export NODE_OPTIONS="--max-old-space-size=7168" #increase to 7gb
 #electron:serve-tsc
 #node_modules/.bin/tsc -p tsconfig.serve.json
 
-npm run node_modules/typescript/bin/tsc -p tsconfig.serve.json && ng build -- -c production && electron-builder build --windows
+npm run node_modules\typescript\bin\tsc -p tsconfig.serve.json && ng build -- -c production && electron-builder build --windows

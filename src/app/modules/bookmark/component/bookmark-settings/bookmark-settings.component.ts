50034e79006e2f3220f67d043914eb1bf4816542
Kevin Strasters <kevin.strasters@gmail.com>
2021-07-21 15:26:32 -0500

    clarify option to add image as bookmark (#228)
    * clarify option to add image as bookmark
    
    * pr comments

--- src/app/modules/bookmark/component/bookmark-settings/bookmark-settings.component.ts
+++ src/app/modules/bookmark/component/bookmark-settings/bookmark-settings.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core'
 import { UserSettings, UserSettingsComponent } from 'src/app/layout/type'
 
 export interface BookmarkUserSettings extends UserSettings {
@@ -20,6 +20,8 @@ export interface BookmarkUserBookmark {
 export class BookmarkSettingsComponent implements UserSettingsComponent {
   @Input()
   public settings: BookmarkUserSettings
+  @ViewChild('ghostInput')
+  public input: ElementRef;
 
   public load(): void {
     // stub
@@ -27,6 +29,19 @@ export class BookmarkSettingsComponent implements UserSettingsComponent {
 
   public onAddClick(): void {
     this.addBookmark()
+  }
+
+  public onAddFileClick(): void {
+    (this.input.nativeElement as HTMLInputElement).click();
+  }
+
+  public onFileSelected(event: InputEvent): void {
+    const path = (event.target as HTMLInputElement).files.item(0).path;
+    this.settings.bookmarks.push({
+      url: `file:///${path}`,
+      shortcut: undefined,
+      external: false,
+    });
   }
 
   public onRemoveClick(index: number): void {

678b4a6bed7c0c549086dd5bd0091bb8b50c1610
WhiteFang <jorisschroeven@hotmail.com>
2021-10-19 19:10:45 +0200

    Moved 'special stats' (i.e. logbook faction) parsing to its own parser service class

--- src/app/shared/module/poe/service/item/parser/item-section-stats-parser.service.ts
+++ src/app/shared/module/poe/service/item/parser/item-section-stats-parser.service.ts
@@ -7,12 +7,7 @@ import {
   ItemSectionParserService,
   Section,
   ItemCategory,
-  ItemStat,
-  StatType,
-  Language,
 } from '@shared/module/poe/type'
-import { StatsProvider } from '../../../provider/stats.provider'
-import { ClientStringService } from '../../client-string/client-string.service'
 import { StatsSearchOptions, StatsService } from '../../stats/stats.service'
 
 @Injectable({
@@ -21,8 +16,6 @@ import { StatsSearchOptions, StatsService } from '../../stats/stats.service'
 export class ItemSectionStatsParserService implements ItemSectionParserService {
   constructor(
     private readonly statsService: StatsService,
-    private readonly clientString: ClientStringService,
-    private readonly statsProvider: StatsProvider,
   ) { }
 
   public optional = true
@@ -53,32 +46,6 @@ export class ItemSectionStatsParserService implements ItemSectionParserService {
       (a, b) => content.indexOf(a.match.text) - content.indexOf(b.match.text)
     )
     target.stats = sorted.map((x) => x.stat)
-
-    // Parse special stats that are present in the 'normal stat section' (i.e. they can't be their own section)
-    const specialStats = this.getSpecialStatPhrases()
-    for (const specialStat of specialStats) {
-      for (const section of item.sections) {
-        for (const line of section.lines) {
-          if (line.indexOf(specialStat.phrase) !== -1) {
-            const tradeStats = this.statsProvider.provide(specialStat.statType)
-            const tradeStat = tradeStats[specialStat.tradeId]
-            const stat: ItemStat = {
-              id: tradeStat.id,
-              mod: tradeStat.mod,
-              option: tradeStat.option,
-              negated: tradeStat.negated,
-              predicateIndex: 0,
-              predicate: '#',
-              type: specialStat.statType,
-              tradeId: specialStat.tradeId,
-              values: [],
-              indistinguishables: undefined,
-            }
-            target.stats.push(stat)
-          }
-        }
-      }
-    }
   }
 
   private createOptions(item: Item): StatsSearchOptions {
@@ -136,34 +103,5 @@ export class ItemSectionStatsParserService implements ItemSectionParserService {
     }
 
     return options
-  }
-
-  private getSpecialStatPhrases(): {
-    phrase: string
-    statType: StatType
-    tradeId: string
-  }[] {
-    return [
-      {
-        phrase: this.clientString.translate('ExpeditionFaction1'),
-        statType: StatType.Pseudo,
-        tradeId: 'pseudo_logbook_faction_druids'
-      },
-      {
-        phrase: this.clientString.translate('ExpeditionFaction2'),
-        statType: StatType.Pseudo,
-        tradeId: 'pseudo_logbook_faction_mercenaries'
-      },
-      {
-        phrase: this.clientString.translate('ExpeditionFaction3'),
-        statType: StatType.Pseudo,
-        tradeId: 'pseudo_logbook_faction_order'
-      },
-      {
-        phrase: this.clientString.translate('ExpeditionFaction4'),
-        statType: StatType.Pseudo,
-        tradeId: 'pseudo_logbook_faction_knights'
-      },
-    ]
   }
 }

e3409a1d6b411a88ea3d047daaf8f2e8a78cb25d
WhiteFang <jorisschroeven@hotmail.com>
2021-11-09 19:12:59 +0100

    Added support for 'empty' and 'crafted' pseudo-modifiers (using the new Mod Generation Type detection)

--- src/app/shared/module/poe/service/item/processor/item-pseudo-processor.service.ts
+++ src/app/shared/module/poe/service/item/processor/item-pseudo-processor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { ModifierType, PSEUDO_MODIFIERS } from '@shared/module/poe/config/pseudo.config'
-import { Item, ItemRarity, ItemStat, StatType } from '@shared/module/poe/type'
+import { Item, ItemCategory, ItemRarity, ItemStat, StatGenType, StatType } from '@shared/module/poe/type'
+import { ItemParserUtils } from '../parser/item-parser.utils'
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,135 @@ export class ItemPseudoProcessorService {
       item.stats = []
     }
 
+    const emptyAndCraftedPseudoMods: ItemStat[] = []
+    if (item.category.startsWith(ItemCategory.Weapon)
+      || item.category.startsWith(ItemCategory.Armour)
+      || item.category.startsWith(ItemCategory.Accessory)
+      || item.category.startsWith(ItemCategory.Jewel)
+      || item.category.startsWith(ItemCategory.Flask)) {
+      emptyAndCraftedPseudoMods.push(...this.addPseudoEmptyModifiers(item))
+      emptyAndCraftedPseudoMods.push(...this.addPseudoCraftedModifiers(item))
+    }
+
+    this.groupPseudoMods(item)
+
+    item.stats.push(...emptyAndCraftedPseudoMods)
+  }
+
+  private addPseudoEmptyModifiers(item: Item): ItemStat[] {
+    const newMods: ItemStat[] = []
+    const maxMods = this.getMaxMods(item)
+    if (maxMods.total == 0) {
+      return newMods
+    }
+
+    // Only include crafted mods when the item is corrupted. Otherwise exclude them since they can easily be replaced or removed using the crafting bench (and this shouldn't count towards the total)
+    const explicitMods = item.stats.filter(x => x.genType !== StatGenType.Unknown && (
+      x.type === StatType.Explicit ||
+      x.type === StatType.Fractured ||
+      x.type === StatType.Veiled ||
+      (x.type === StatType.Crafted && item.corrupted)
+    ))
+
+    const numExplicitMods = explicitMods.length
+    const numEmptyAffixMods = maxMods.total - numExplicitMods
+
+    if (numExplicitMods > 0 && numEmptyAffixMods > 0) {
+      newMods.push(this.createPseudoMod('pseudo_number_of_empty_affix_mods', [numEmptyAffixMods]))
+
+      const numPrefixes = explicitMods.filter(x => x.genType === StatGenType.Prefix).length
+      const numSuffixes = explicitMods.filter(x => x.genType === StatGenType.Suffix).length
+      const numEmptyPrefixes = maxMods.prefixes - numPrefixes
+      const numEmptySuffixes = maxMods.suffixes - numSuffixes
+      if (numEmptyPrefixes > 0) {
+        newMods.push(this.createPseudoMod('pseudo_number_of_empty_prefix_mods', [numEmptyPrefixes]))
+      }
+      if (numEmptySuffixes > 0) {
+        newMods.push(this.createPseudoMod('pseudo_number_of_empty_suffix_mods', [numEmptySuffixes]))
+      }
+    }
+    return newMods
+  }
+
+  private addPseudoCraftedModifiers(item: Item): ItemStat[] {
+    const newMods: ItemStat[] = []
+
+    const craftedMods = item.stats.filter(x => x.genType !== StatGenType.Unknown && x.type === StatType.Crafted)
+    const numCraftedMods = craftedMods.length
+
+    if (numCraftedMods > 0) {
+      newMods.push(this.createPseudoMod('pseudo_number_of_crafted_mods', [numCraftedMods]))
+
+      const numPrefixes = craftedMods.filter(x => x.genType === StatGenType.Prefix).length
+      const numSuffixes = craftedMods.filter(x => x.genType === StatGenType.Suffix).length
+      if (numPrefixes > 0) {
+        newMods.push(this.createPseudoMod('pseudo_number_of_crafted_prefix_mods', [numPrefixes]))
+      }
+      if (numSuffixes > 0) {
+        newMods.push(this.createPseudoMod('pseudo_number_of_crafted_suffix_mods', [numSuffixes]))
+      }
+    }
+
+    return newMods
+  }
+
+  private getMaxMods(item: Item): { prefixes: number, suffixes: number, total: number } {
+    const maxMods = {
+      prefixes: 0,
+      suffixes: 0,
+      total: 0
+    }
+
+    switch (item.rarity) {
+      case ItemRarity.Magic:
+        maxMods.prefixes = 1
+        maxMods.suffixes = 1
+        maxMods.total = 2
+        break
+
+      case ItemRarity.Rare:
+        maxMods.prefixes = 3
+        maxMods.suffixes = 3
+        maxMods.total = 6
+        break
+    }
+
+    if (item.category.startsWith(ItemCategory.Jewel)) {
+        maxMods.prefixes = 2
+        maxMods.suffixes = 2
+        maxMods.total = 4
+    }
+
+    const maxPrefixesPlus = item.stats.find(x => x.id === 'local_maximum_prefixes_allowed_+')
+    if (maxPrefixesPlus) {
+      maxMods.prefixes += maxPrefixesPlus.values[0].value
+    }
+
+    const maxSuffixesPlus = item.stats.find(x => x.id === 'local_maximum_suffixes_allowed_+')
+    if (maxSuffixesPlus) {
+      maxMods.suffixes += maxSuffixesPlus.values[0].value
+    }
+
+    return maxMods
+  }
+
+  private createPseudoMod(id: string, values: number[]): ItemStat {
+    const itemStat: ItemStat = {
+      id,
+      type: StatType.Pseudo,
+      predicateIndex: 0,
+      predicate: '#',
+      tradeId: id,
+      values: values.map(x => ({ text: `${+x.toFixed(2)}` })),
+      negated: false,
+      option: false,
+      mod: undefined,
+      indistinguishables: undefined,
+    }
+    return itemStat
+  }
+
+  private groupPseudoMods(item: Item): void {
     const itemStats = [...item.stats]
     Object.getOwnPropertyNames(PSEUDO_MODIFIERS).forEach((id) => {
       const pseudo = PSEUDO_MODIFIERS[id]
@@ -59,18 +189,7 @@ export class ItemPseudoProcessorService {
         }
       }
 
-      const itemStat: ItemStat = {
-        id,
-        type: StatType.Pseudo,
-        predicateIndex: 0,
-        predicate: '#',
-        tradeId: id,
-        values: values.map((x: number) => ({ text: `${+x.toFixed(2)}` })),
-        negated: false,
-        option: false,
-        mod: undefined,
-        indistinguishables: undefined,
-      }
+      const itemStat = this.createPseudoMod(id, values)
       itemStats.push(itemStat)
       if (values.length > 0 && (!minCount || count >= minCount)) {
         item.stats.push(itemStat)
